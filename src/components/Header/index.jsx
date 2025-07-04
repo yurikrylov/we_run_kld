97ff0f1359822d812a963dc27baf7c9e0c5f5c24
Yuri Krylov <yuri.krylov@gmail.com>
2025-07-04 20:51:21 +0200

    небольшие доработки

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import {Box, Grid, Link} from '@mui/material';
 import Typography from '@mui/material/Typography';
-
+import TelegramIcon from '@mui/icons-material/Telegram';
 
 const Header = () => {
   return (
     <Box sx={{ width: '100%' }}>
-      <Typography variant="h1" gutterBottom sx={{ fontWeight: 'medium' }}>
-        WE RUN KLD
-      </Typography>
-      </Box>
+      <Grid container spacing={3}>
+        <Grid size={{ xs: 9 }}>
+          <Typography variant="h1" gutterBottom sx={{ fontWeight: 'medium' }}>
+            WE RUN KLD
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 3 }} >
+          <Typography variant="h1" gutterBottom sx={{ fontWeight: 'medium' }}>
+
+            <Link href='https://t.me/+urybSLBV1ws3YmRi'>{<TelegramIcon />}</Link>
+          </Typography>
+        </Grid>
+      </Grid>
+
+    </Box >
   )
 }
 
--- src/components/RouteCard/index.jsx
+++ src/components/RouteCard/index.jsx
@@ -1,4 +1,4 @@
-import {Card, CardActions, CardContent, CardMedia, Typography, Link, Button} from '@mui/material';
+import { Card, CardActions, CardContent, CardMedia, Typography,  Button } from '@mui/material';
 
 export default function RouteCard({route}) {
 
@@ -7,21 +7,27 @@ export default function RouteCard({route}) {
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image= {route.photolink}
+        image={route.photolink}
         title="фото маршрута"
       />
       <CardContent>
         <Typography sx={{ height: 80 }} gutterBottom variant="h5" component="div">
           {route.name}
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary', height:80, overflow:'auto'} }>
+        <Typography variant="body2" sx={{ color: 'text.secondary', height: 80, overflow: 'auto' }}>
           {route.desc}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary', height: 80, overflow: 'auto' }}>
+          Расстояние: {route.distance} км
+        </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary', height: 80, overflow: 'auto' }}>
+          Светофоров: {route.trafficlight} 
         </Typography>
       </CardContent>
       <CardActions sx={{ justifyContent: 'space-between' }}>
-                <Button         sx={{ height: 80 }} href={null} variant="text">ОТКРЫТЬ</Button>
+        <Button sx={{ height: 80 }} href={null} variant="text">ОТКРЫТЬ</Button>
 
-        <Button         sx={{ height: 80 }} href={route.maplink} variant="text">КАРТА</Button>
+        <Button sx={{ height: 80 }} href={route.maplink} variant="text">КАРТА</Button>
       </CardActions>
     </Card>
   );
